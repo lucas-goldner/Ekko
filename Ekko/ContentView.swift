a200d69de5c66a48e9b7da6774f693f5a464f3cd
lucas-goldner <lucas.goldner@googlemail.com>
2023-04-08 23:52:44 +0200

    feat: Add fetching of current album cover

--- Ekko/ContentView.swift
+++ Ekko/ContentView.swift
@@ -13,9 +13,18 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "music.note")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            if let artwork = musicController.currentSongArtwork {
+                Image(uiImage: artwork)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                    .shadow(radius: 7)
+            } else {
+                Image(systemName: "music.note")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
             Text(musicController.currentSongTitle ?? "No Song Playing")
             Button(action: {}) {
                 Image(systemName: "backward.fill")
--- Ekko/MusicController.swift
+++ Ekko/MusicController.swift
@@ -10,20 +10,25 @@ import MediaPlayer
 
 class MusicController: ObservableObject {
     @Published var currentSongTitle: String?
+    @Published var currentSongArtwork: UIImage?
 
     func checkForCurrentSong() {
         NotificationCenter.default.addObserver(forName: .MPMusicPlayerControllerNowPlayingItemDidChange, object: nil, queue: .main) { [self] notification in
             if let nowPlaying = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem {
                 self.currentSongTitle = nowPlaying.title
+                self.currentSongArtwork = nowPlaying.artwork?.image(at: CGSize(width: 200, height: 200))
             } else {
                 self.currentSongTitle = nil
+                self.currentSongArtwork = nil
             }
         }
         
         if let nowPlaying = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem {
             currentSongTitle = nowPlaying.title
+            currentSongArtwork = nowPlaying.artwork?.image(at: CGSize(width: 200, height: 200))
         } else {
             currentSongTitle = nil
+            currentSongArtwork = nil
         }
     }
 }
